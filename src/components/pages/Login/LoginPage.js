dcbe155b61f27316d26b094e0123b73a295ab75c
Alecsandra Brewer <alecsandrabrewer@yahoo.com>
2020-09-22 01:01:28 -0700

    login page done

--- src/components/pages/Login/LoginPage.js
+++ src/components/pages/Login/LoginPage.js
@@ -1,1 +1,38 @@
-export { default as LoginPage } from './LoginContainer';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const initialLogin = {
+    email: '',
+    password: ''
+}
+
+const LoginApp = () => {
+    const [login, setLogin] = useState(initialLogin);
+
+    //onChange Handler
+    const changeHandler = (evt) => {
+        setFormValue(evt.target.value)
+    };
+
+    //onSubmit Handler
+    const submitHandler = (evt) => {
+        evt.preventdefault()
+    }
+
+    return(
+        <form onSubmit={submitHandler}>
+            <label>
+                Email
+                <input type='text' name='email' value={login.email} onChange={changeHandler}></input>
+            </label>
+            <label>
+                Password 
+                <input type='text' name='password' value={login.password} onChange={changeHandler}></input>
+            </label>
+
+            <button>Log In</button>
+        </form>
+    )
+}
+
+export default LoginPage;
