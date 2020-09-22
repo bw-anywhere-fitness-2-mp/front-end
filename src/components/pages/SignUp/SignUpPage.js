f70d1fb513973c53ebca6d74477fa88a4525d9f3
Alecsandra Brewer <alecsandrabrewer@yahoo.com>
2020-09-22 13:46:12 -0700

    sstyling in progress

--- src/components/pages/SignUp/SignUpPage.js
+++ src/components/pages/SignUp/SignUpPage.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import styled from 'styled-components';
 
 const initialFormValues = {
   email: '',
@@ -8,6 +9,41 @@ const initialFormValues = {
   client: true,
   instructor: false,
 };
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #dcdcdc;
+  height: 99vh;
+`;
+const Header = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  background-color: black;
+  width: 100%;
+  height: 10vh;
+  color: white;
+`;
+const Navigation = styled.div`
+  padding: 2%;
+  margin: 1%;
+  &:hover {
+    background-color: white;
+    color: black;
+  }
+`;
+const SignUpContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  padding: 5%;
+  border: 5px solid black;
+  width: 60%;
+  margin: 10%;
+  background-color: #778899;
+  color: white;
+`;
 
 const SignUpPage = () => {
   const [formValue, setFormValue] = useState(initialFormValues);
@@ -30,45 +66,69 @@ const SignUpPage = () => {
   };
 
   return (
-    <form onSubmit={submitHandler}>
-      <h3>Sign Up!</h3>
-      <label>
-        Email
-        <input
-          type="text"
-          name="email"
-          value={formValue.email}
-          onChange={changeHandler}
-          placeholder="Email"
-        ></input>
-      </label>
-      <label>
-        Name
-        <input
-          type="text"
-          name="name"
-          value={formValue.name}
-          onChange={changeHandler}
-          placeholder="Full Name"
-        ></input>
-      </label>
-      <label>
-        Create Your password
-        <input
-          type="text"
-          name="password"
-          value={formValue.password}
-          onChange={changeHandler}
-          placeholder="Password"
-        ></input>
-      </label>
-      <select id="customerType" name="customerType">
-        <option value="client">Client</option>
-        <option value="instructor">Instructor</option>
-      </select>
+    <Container>
+      <Header className="headerNav">
+        <Navigation>
+          <nav>HOME</nav>
+        </Navigation>
+        <Navigation>
+          <nav>ABOUT</nav>
+        </Navigation>
+        <Navigation>
+          <nav>REGISTRATION</nav>
+        </Navigation>
+        <Navigation>
+          <nav>SIGN IN</nav>
+        </Navigation>
+      </Header>
 
-      <button>Submit</button>
-    </form>
+      <SignUpContainer className="signUpForm">
+        <form onSubmit={submitHandler}>
+          <h3>SIGN UP</h3>
+          <label>
+            Email
+            <input
+              type="text"
+              name="email"
+              value={formValue.email}
+              onChange={changeHandler}
+              placeholder="Email"
+            ></input>
+          </label>
+          <br></br>
+          <label>
+            Name
+            <input
+              type="text"
+              name="name"
+              value={formValue.name}
+              onChange={changeHandler}
+              placeholder="Full Name"
+            ></input>
+          </label>
+          <br></br>
+          <label>
+            Create Your password
+            <input
+              type="text"
+              name="password"
+              value={formValue.password}
+              onChange={changeHandler}
+              placeholder="Password"
+            ></input>
+          </label>
+          <br></br>
+          <select id="customerType" name="customerType">
+            <option value="default">-Select-</option>
+            <option value="client">Client</option>
+            <option value="instructor">Instructor</option>
+          </select>
+          <br></br>
+
+          <button>Submit</button>
+        </form>
+      </SignUpContainer>
+    </Container>
   );
 };
 
