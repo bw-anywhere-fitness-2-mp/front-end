b2c983e00d63c146c6525dddefdf7cf589599f72
Alecsandra Brewer <alecsandrabrewer@yahoo.com>
2020-09-22 00:27:44 -0700

    most of sign up form done

---
+++ src/components/pages/SignUp/SignUpPage.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+
+const initialFormValues = {
+    email: '',
+    name: '',
+    password: '',
+    client: true,
+    instructor: false
+}
+
+const SignUpApp = () => {
+    const [formValue, setFormValue] = useState(initialFormValues);
+
+    //onChange Handler
+    const changeHandler = (evt) => {
+        setFormValue(evt.target.value)
+    };
+
+    //onSubmit Handler
+    const submitHandler = (evt) => {
+        evt.preventdefault()
+        const newCustomer = {
+            emai: formValue.email.trim(),
+            name: formValue.name.trim(),
+            password: formValue.password.trim(),
+            client: formValue.client.trim(),
+            instructor: formValue.instructor.trim()
+        }
+    }
+
+    return(
+        <form onSubmit={}>
+            <h3>Sign Up!</h3>
+            <label>
+                Email
+                <input type='text' name='email' value={formValue.email} onChange={} placeholder='Email'></input>
+            </label>
+            <label>
+                Name
+                <input type='text' name='name' value={formValue.name} onChange={} placeholder='Full Name'></input>
+            </label>
+            <label>
+                Create Your password
+                <input type='text' name='password' value={formValue.password} onChange={} placeholder='Password'></input>
+            </label>
+            <select id='customerType' name='customerType'>
+                <option value='client'>Client</option>
+                <option value='instructor'>Instructor</option>
+            </select>
+
+            <button>Submit</button>
+        </form>
+    )
+}
+
+export default SignUpPage;
